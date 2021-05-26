0a373616262dbec2f78c842e45fcf507a087b698
aishwarya-bm <biradaraishwarya@gmail.com>
2021-05-26 16:48:51 +0530

    added trip card for details

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import { useState } from 'react';
 import './App.css';
 import trip from "./assets/trip.svg"
+import TripCard from './TripCard';
 
 function App() {
-  const earphones = ["skullcandy","jbl","boat"]
-  const laptops = ["lenovo","dell","hp"]
-  const mobiles = ["iphone","oneplus","samsung"]
-
  
-  const friends = ["Goa","Spiti valley","Kasol"]
-  const famjam = ["Bhutan","Andaman Island","Rajasthan"]
-  const road = ["Delhi to Leh","Vizag to Araku Valley", "Chennai to Pondicherry"]
-  const cruise = ["The Carribean", "Alaska","Hawaii","The Mediterranean"]
+  const friends = [
+    {
+      name:"Mount Abu",do : "Hiking, rock climbing", "stay": "Udai Garh Hotel, Hotel Banjara", budget: "INR 10,000"
+    },
+    {
+      name:"Rishikesh",do : "River rafting,camping, bungee jumping", "stay": "Swiss Cottage and Spa By Salvus,Aloha by Ganges", budget: "INR 10,000"
+    },
+    {
+      name:"Roopkund Lake",do : "Trekking, camping, rappelling", "stay": "Patwal Hotel, Nature’s Treat Resort", budget: "INR 4,000"
+    }
+  ]
+  const famjam = [
+    {
+      name : "Dharamshala, Himachal Pradesh", food : "Momos, Tibetan food", eatAt : "Illiterati, Woeser Bakery, Tibet Kitchen"
+    },
+    {
+      name : "Shillong, Megahlaya", food : "Momos, Tungrymbai, Chowmein", eatAt : "The Lounge, Jiva Grill"
+    },
+    {
+      name : "Gangtok, Sikkim", food : "Thukpa, Gundruk, Phagshapa", eatAt : "Cafe Live & Loud, The Square"
+    }
+  ]
 
+  const cruise = [
+    {
+      name : "Andaman Islands Cruise", location: "Andaman", duration: "5 Days/4 Nights", price: "INR 17,000 onwards"
+    },
+    {
+      name : "Kochi Cruise", location: "Kochi", duration: "2 hours", price: "INR 1,000 onwards"
+    },
+    {
+      name : "Kerala Backwater Cruise", location: "Alleppey, Kerala", duration: "3 Days/2 Nights", price: "INR 5,000 onwards"
+    }
+  ]
+  const road = [
+    {
+      name : "Chennai to Pondicherry", dist : "158kms", time: "3-4 hrs",highways : "NH34",bestmonths : "Oct to Mar"
+    },
+    {
+      name : "Delhi to Leh", dist : "1091kms", time: "25 hrs",highways : "NH1, NH2",bestmonths : "May to Sep"
+    },
+    {
+      name : "Vizag to Araku Valley", dist : "116kms", time: "3-4 hrs",highways : "NA",bestmonths : "Dec to feb"
+    }
+  ]
 
   const [suggestions,setSuggestions] = useState([]);
   const [selected,setSelected] = useState("");
@@ -35,6 +72,7 @@ function App() {
 
   return (
     <div >
+      <div style={{textAlign: "center"}}>
         <img src = {trip} alt="logo" style={{height : "175px"}}></img>
         <h4 style = {{margin : "10px 0px",color:"white"}}>Check out some suggestions here for your trips. Select a category.</h4>
         
@@ -52,10 +90,11 @@ function App() {
           onClick = {() => btnClickHandler("cruise")}
           >Cruise</button>
         </div>
+        </div>
         <hr style={{color:'black'}}></hr>
         <div className = "suggested-list">
           {suggestions.map(item=>{
-            return <div key={item} className = "suggested-item">{item}</div>
+            return <TripCard key={item} tripItem = {item} selected = {selected} />
           })}
         </div>      
     </div>
---
+++ src/TripCard.js
@@ -0,0 +1,43 @@
+import './App.css';
+
+function TripCard(props) {
+    if(props.selected === "road")
+        return(
+                <section className="suggested-item">
+                    <div style={{fontWeight : "bold"}}>{props.tripItem.name}</div>
+                    <div>Distance : {props.tripItem.dist}</div>
+                    <div>Time : {props.tripItem.time}</div>
+                    <div>Highways : {props.tripItem.highways}</div>
+                    <div>Best time to visit : {props.tripItem.bestmonths}</div>
+                </section>
+        );
+    else if(props.selected === "cruise")
+        return(
+                <section className="suggested-item">
+                    <div style={{fontWeight : "bold"}}>{props.tripItem.name}</div>
+                    <div>Location : {props.tripItem.location}</div>
+                    <div>Duration : {props.tripItem.duration}</div>
+                    <div>Price : {props.tripItem.price}</div>
+                </section>
+        );
+    else if(props.selected === "famjam")
+        return(
+                <section className="suggested-item">
+                    <div style={{fontWeight : "bold"}}>{props.tripItem.name}</div>
+                    <div>Famous food : {props.tripItem.food}</div>
+                    <div>Eat at : {props.tripItem.eatAt}</div>
+                </section>
+        );
+    else
+        return(
+            <section className="suggested-item">
+                    <div style={{fontWeight : "bold"}}>{props.tripItem.name}</div>
+                    <div>Things to do : {props.tripItem.do}</div>
+                    <div>Stay at : {props.tripItem.stay}</div>
+                    <div>Budget : {props.tripItem.budget}</div>
+                </section>
+        )        
+
+}
+
+export default TripCard;
