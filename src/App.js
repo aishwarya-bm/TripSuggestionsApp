9c38fda2071d020c546c5f36b16829cfbec3776c
aishwarya-bm <66695068+aishwarya-bm@users.noreply.github.com>
2021-09-02 00:23:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,47 +7,47 @@ function App() {
  
   const friends = [
     {
-      name:"Mount Abu",do : "Hiking, rock climbing", "stay": "Udai Garh Hotel, Hotel Banjara", budget: "INR 10,000"
+      name:"Mount Abu",do : "Hiking, rock climbing", "stay": "Udai Garh Hotel, Hotel Banjara", budget: "INR 10,000", rating: "3.5/5"
     },
     {
-      name:"Rishikesh",do : "River rafting,camping, bungee jumping", "stay": "Swiss Cottage and Spa By Salvus,Aloha by Ganges", budget: "INR 10,000"
+      name:"Rishikesh",do : "River rafting,camping, bungee jumping", "stay": "Swiss Cottage and Spa By Salvus,Aloha by Ganges", budget: "INR 10,000", rating: "4/5"
     },
     {
-      name:"Roopkund Lake",do : "Trekking, camping, rappelling", "stay": "Patwal Hotel, Nature’s Treat Resort", budget: "INR 4,000"
+      name:"Roopkund Lake",do : "Trekking, camping, rappelling", "stay": "Patwal Hotel, Nature’s Treat Resort", budget: "INR 4,000", rating: "4.5/5"
     }
   ]
   const famjam = [
     {
-      name : "Dharamshala, Himachal Pradesh", food : "Momos, Tibetan food", eatAt : "Illiterati, Woeser Bakery, Tibet Kitchen"
+      name : "Dharamshala, Himachal Pradesh", food : "Momos, Tibetan food", eatAt : "Illiterati, Woeser Bakery, Tibet Kitchen", rating: "4/5"
     },
     {
-      name : "Shillong, Megahlaya", food : "Momos, Tungrymbai, Chowmein", eatAt : "The Lounge, Jiva Grill"
+      name : "Shillong, Megahlaya", food : "Momos, Tungrymbai, Chowmein", eatAt : "The Lounge, Jiva Grill", rating: "4.5/5"
     },
     {
-      name : "Gangtok, Sikkim", food : "Thukpa, Gundruk, Phagshapa", eatAt : "Cafe Live & Loud, The Square"
+      name : "Gangtok, Sikkim", food : "Thukpa, Gundruk, Phagshapa", eatAt : "Cafe Live & Loud, The Square", rating: "3/5"
     }
   ]
 
   const cruise = [
     {
-      name : "Andaman Islands Cruise", location: "Andaman", duration: "5 Days/4 Nights", price: "INR 17,000 onwards"
+      name : "Andaman Islands Cruise", location: "Andaman", duration: "5 Days/4 Nights", price: "INR 17,000 onwards", rating: "3/5"
     },
     {
-      name : "Kochi Cruise", location: "Kochi", duration: "2 hours", price: "INR 1,000 onwards"
+      name : "Kochi Cruise", location: "Kochi", duration: "2 hours", price: "INR 1,000 onwards", rating: "4.5/5"
     },
     {
-      name : "Kerala Backwater Cruise", location: "Alleppey, Kerala", duration: "3 Days/2 Nights", price: "INR 5,000 onwards"
+      name : "Kerala Backwater Cruise", location: "Alleppey, Kerala", duration: "3 Days/2 Nights", price: "INR 5,000 onwards", rating: "4/5"
     }
   ]
   const road = [
     {
-      name : "Chennai to Pondicherry", dist : "158kms", time: "3-4 hrs",highways : "NH34",bestmonths : "Oct to Mar"
+      name : "Chennai to Pondicherry", dist : "158kms", time: "3-4 hrs",highways : "NH34",bestmonths : "Oct to Mar", rating: "4.5/5"
     },
     {
-      name : "Delhi to Leh", dist : "1091kms", time: "25 hrs",highways : "NH1, NH2",bestmonths : "May to Sep"
+      name : "Delhi to Leh", dist : "1091kms", time: "25 hrs",highways : "NH1, NH2",bestmonths : "May to Sep", rating: "4/5"
     },
     {
-      name : "Vizag to Araku Valley", dist : "116kms", time: "3-4 hrs",highways : "NA",bestmonths : "Dec to feb"
+      name : "Vizag to Araku Valley", dist : "116kms", time: "3-4 hrs",highways : "NA",bestmonths : "Dec to feb", rating: "3/5"
     }
   ]
 
